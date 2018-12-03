b42d082c1395367728f949c06e0a9914dbb41705
Your Name <you@example.com>
2018-12-02 18:18:09 -0600

    added tuning class to index.js file

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+class Tunings {
+	// Once a tuning is selected from the drop down menu
+	// each method here will switch out the corresponding audio for that tuning 
+	standard() {
+		var note1 = document.getElementById('E');
+		var note2 = document.getElementById('A');
+		var note3 = document.getElementById('D');
+		var note4 = document.getElementById('G');
+		var note5 = document.getElementById('B');
+		var note6 = document.getElementById('E-1');
+
+			note1.src = "Audio/tunings/Standard/Standard E.wav";
+			note2.src = "Audio/tunings/Standard/Standard A.wav";
+			note3.src = "Audio/tunings/Standard/Standard D.wav";
+			note4.src = "Audio/tunings/Standard/Standard G.wav";
+			note5.src = "Audio/tunings/Standard/Standard B.wav";
+			note6.src = "Audio/tunings/Standard/Standard E-1.wav";
+
+			note1.load()
+			note2.load()
+			note3.load()
+			note4.load()
+			note5.load()
+			note6.load()
+	}
+	dropD() {
+		var note1 = document.getElementById('E');
+		var note2 = document.getElementById('A');
+		var note3 = document.getElementById('D');
+		var note4 = document.getElementById('G');
+		var note5 = document.getElementById('B');
+		var note6 = document.getElementById('E-1');
+
+			note1.src = "Audio/tunings/Standard/Standard D.wav";
+			note2.src = "Audio/tunings/Standard/Standard A.wav";
+			note3.src = "Audio/tunings/Standard/Standard D.wav";
+			note4.src = "Audio/tunings/Standard/Standard E-1.wav";
+			note5.src = "Audio/tunings/Standard/Standard G.wav";
+			note6.src = "Audio/tunings/Standard/Standard B.wav";
+
+			note1.load()
+			note2.load()
+			note3.load()
+			note4.load()
+			note5.load()
+			note6.load()
+	}
+	openC() {
+		var note1 = document.getElementById('E');
+		var note2 = document.getElementById('A');
+		var note3 = document.getElementById('D');
+		var note4 = document.getElementById('G');
+		var note5 = document.getElementById('B');
+		var note6 = document.getElementById('E-1');
+
+			note1.src = "Audio/tunings/Standard/Standard D.wav";
+			note2.src = "Audio/tunings/Standard/Standard D.wav";
+			note3.src = "Audio/tunings/Standard/Standard D.wav";
+			note4.src = "Audio/tunings/Standard/Standard D.wav";
+			note5.src = "Audio/tunings/Standard/Standard D.wav";
+			note6.src = "Audio/tunings/Standard/Standard E-1.wav";
+
+			note1.load()
+			note2.load()
+			note3.load()
+			note4.load()
+			note5.load()
+			note6.load()
+	}
+}
+
+var tunings = new Tunings() // creates instance of the class
+
 function playNote(id) {
 	var audio = document.getElementById(id);
     if (audio.paused == true) {
@@ -8,17 +81,13 @@ function playNote(id) {
     }
 }
 
-function dropD() {
-	var note = document.getElementById('E');
-		note.src = "Audio/tunings/Standard/Standard D.wav";
-		note.load()
-}
 
 function changeTuning() {
+	// Changes the letters of each button according to what tuning is selected
 	var val = document.getElementById('tunings').value;
-
 	// stanard tuning	
 	if (val == 'standard') {
+		tunings.standard()
 		document.getElementById('note1').innerHTML = "E";
 		document.getElementById('note2').innerHTML = "A";
 		document.getElementById('note3').innerHTML = "D";
@@ -27,7 +96,7 @@ function changeTuning() {
 		document.getElementById('note6').innerHTML = "E";
 	// drop D tuning
 	} else if (val == 'dropD') {
-		dropD()
+		tunings.dropD()
 		document.getElementById('note1').innerHTML = "D";
 		document.getElementById('note2').innerHTML = "A";
 		document.getElementById('note3').innerHTML = "D";
@@ -36,6 +105,7 @@ function changeTuning() {
 		document.getElementById('note6').innerHTML = "E";
 	// open C tuning
 	} else if (val == 'openC') {
+		tunings.openC()
 		document.getElementById('note1').innerHTML = "C";
 		document.getElementById('note2').innerHTML = "G";
 		document.getElementById('note3').innerHTML = "C";

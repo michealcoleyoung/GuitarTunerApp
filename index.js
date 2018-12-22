11ac303ce945d13fe60309cc670ab19969484068
Your Name <you@example.com>
2018-12-22 16:58:11 -0600

    test comment added

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ var tunings = new Tunings() // creates instance of the class
 
 function playNote(id) {
 	// play and pause
-	// changes color of button when it's active
+	// shows active notes being played
 	var audio = document.getElementById(id);
     if (audio.paused == true) {
     	var notes = document.getElementsByClassName('notes');
